24322755e39ce8a8712b960ec8cfa64dd3c7dc41
David J Ortiz <87109541+MariachiMES@users.noreply.github.com>
2021-08-21 00:57:23 -0500

    next button now working to ppopulate new question

--- script.js
+++ script.js
@@ -5,10 +5,13 @@ var choice1El = document.querySelector("#choice1");
 var choice2El = document.querySelector("#choice2");
 var choice3El = document.querySelector("#choice3");
 var choice4El = document.querySelector("#choice4");
-var answerButtonEl = document.querySelectorAll("answerBtns");
+var answerButtonEl = document.querySelectorAll(".answerBtns");
 var scoreEL = document.querySelector("#score");
 var gamerHeaderEl = document.querySelector(".gameHeader");
 var buttonConEl = document.querySelector(".button-container");
+var nextBtnEl = document.querySelector("#next-button");
+var nextButtonEl = document.querySelector("#next-button");
+var proceedEl = document.querySelector(".proceed");
 
 var timeEl = document.querySelector(".time");
 var mainEl = document.getElementById("main");
@@ -47,6 +50,7 @@ var gameOver = function () {
   buttonConEl.setAttribute("style", "display: none");
   timeEl.innerHTML = "";
   mainEL.innerHTML = "";
+  proceedEl.setAttribute("style", "display:none");
 };
 
 // Function to create and append colorsplosion image
@@ -112,29 +116,30 @@ var questionArray = [
     correctAnswer: "C#",
   },
 ];
-
+var questionArrayIndex = 0;
 //use the DOM to change the text
 var getQuestion = function () {
-  questionNumEl.innerHTML = questionArray[0].questionNum;
-  questionEl.innerHTML = questionArray[0].question;
-  choice1El.innerHTML = questionArray[0].choice1;
-  choice2El.innerHTML = questionArray[0].choice2;
-  choice3El.innerHTML = questionArray[0].choice3;
-  choice4El.innerHTML = questionArray[0].choice4;
+  questionNumEl.innerHTML = questionArray[questionArrayIndex].questionNum;
+  questionEl.innerHTML = questionArray[questionArrayIndex].question;
+  choice1El.innerHTML = questionArray[questionArrayIndex].choice1;
+  choice2El.innerHTML = questionArray[questionArrayIndex].choice2;
+  choice3El.innerHTML = questionArray[questionArrayIndex].choice3;
+  choice4El.innerHTML = questionArray[questionArrayIndex].choice4;
 };
-
 var startQuiz = function () {
   getQuestion();
 };
+
 var displayQuestion = function () {
   //Is the choice the correct answer
-  if (this.innerHTML === questionArray[0].correctAnswer) {
+  if (this.innerHTML === questionArray[questionArrayIndex].correctAnswer) {
     this.innerHTML = "CORRECT";
     this.setAttribute("style", "background-color: #00FF00");
     score += 10;
     scoreEL.innerHTML = "SCORE: " + score;
     console.log("your score is " + score);
-    nextQuestion();
+    nextButtonEl.innerHTML = "NEXT";
+    questionArrayIndex++;
   } else {
     console.log("incorrect");
     secondsLeft -= 5;
@@ -142,9 +147,9 @@ var displayQuestion = function () {
     this.setAttribute("style", "background-color: #FF0000");
   }
   //If not, time is deducted from the time for a wrong answer
-
-  //deduct time from the timer for incorrect answers
 };
+//deduct time from the timer for incorrect answers
+nextButtonEl.addEventListener("click", startQuiz);
 
 //When the user clicks on an answer choice
 choice1El.addEventListener("click", displayQuestion);
@@ -155,29 +160,6 @@ choice4El.addEventListener("click", displayQuestion);
 //chage the color of the
 
 //go to next question
+//go to high score screen
 
-var nextQuestion = function () {
-  for (var i = 0; i < questionArray.length; i++) {
-    if (this.innerHTML === questionArray[i].correctAnswer) {
-      this.innerHTML = "CORRECT";
-      this.setAttribute("style", "background-color: #00FF00");
-      score += 10;
-      scoreEL.innerHTML = "SCORE: " + score;
-      console.log("your score is " + score);
-      questionNumEl.innerHTML = questionArray[i].questionNum;
-      questionEl.innerHTML = questionArray[i].question;
-      choice1El.innerHTML = questionArray[i].choice1;
-      choice2El.innerHTML = questionArray[i].choice2;
-      choice3El.innerHTML = questionArray[i].choice3;
-      choice4El.innerHTML = questionArray[i].choice4;
-      nextQuestion();
-    } else {
-      console.log("incorrect");
-      secondsLeft -= 5;
-      this.innerHTML = "INCORRECT";
-      this.setAttribute("style", "background-color: #FF0000");
-      nextQuestion();
-    }
-  }
-};
 startQuiz();
